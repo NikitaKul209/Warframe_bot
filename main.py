53f4c93708624cfe622ed96065b48e757449de70
NikitaKul209 <Nikita2099n@mail.ru>
2023-08-08 08:19:43 +0300

    new functions

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from telebot import types
 import datetime
 from datetime import datetime
 import pytz
+import schedule
+import time
+from threading import Thread
 import googletrans
 from googletrans import Translator
 from deep_translator import (GoogleTranslator,
@@ -25,6 +28,8 @@ all_missions = []
 cycle = []
 events = []
 event_info = []
+subscribers = {}
+notification_schedule={}
 def get_events():
     event_info = []
     events = []
@@ -43,14 +48,11 @@ def get_events():
         remaining_days = time_left.days
         remaining_hours, remainder = divmod(time_left.seconds, 3600)
         remaining_minutes, _ = divmod(remainder, 60)
-        remaining_time = (f"До конца ивента осталось:\nДней: {remaining_days}\nЧасов: {remaining_hours}\nМинут: {remaining_minutes}")
-
-
-
-
-
-        event_info.append((f"\n{events[i]['description']}\nЛокация: {events[i]['node']}\nНаграда: {events[i]['rewards'][0]['asString']}\n{remaining_time}\n"))
+        remaining_time = (f"*До конца ивента осталось*:\nДней: {remaining_days} | Часов: {remaining_hours} | Минут: {remaining_minutes}")
+        event_info.append((f"\n*{events[i]['description']}*\n*Локация: *{events[i]['node']}\n*Награда: *{events[i]['rewards'][0]['asString']}\n{remaining_time}\n"))
     return event_info
+
+
 def get_mods(name):
 
     url = f"https://api.warframestat.us/items/search/{name}"
@@ -65,6 +67,7 @@ def get_mods(name):
 def print_data(data, indent=0):
     output = ""  # Переменная для сохранения вывода
     # Выводим 'name', 'description' и 'type' сначала
+    print(data)
     if 'name' in data:
         output += "Name: " + data['name'] + "\n"
     if 'description' in data:
@@ -88,36 +91,34 @@ def print_data(data, indent=0):
     return output  # Возвращаем результат вместо вывода на экран
 
 
-def get_item(name):
-    msg = name
-    name = name.text
-
+def get_item(message):
+    msg = message
+    name = message.text
+    if name.lower() == 'назад':
+        get_text_messages(message)
+        return
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    # next_item = types.KeyboardButton("Искать следующий предмет")
+    btn1 = types.KeyboardButton("Назад")
+    markup.add(btn1)
     url = f"https://api.warframestat.us/items/{name}"
     params = {'only':["name,description,attacks,type"],'language':'ru',}
     response = requests.get(url,params=params)
     response.headers.get("Content-Type")
     data = response.json()
     items = print_data(data)
+    bot.send_message(msg.from_user.id, items, reply_markup=markup )
+    bot.register_next_step_handler(message, get_item)
+    # bot.register_next_step_handler(message, process_search_choice)
 
-    bot.send_message(msg.from_user.id, items)
-
-
-
-    print(items)
     return items
 
-    # item = (f"Наименование:*{data['name']}\n*{data['description']}\n\nХарактеристики\n:"
-    #         f"\nШанс крита: {data['attacks'][0]['crit_chance']}"
-    #         f"\nКрит урон: {data['attacks'][0]['crit_mult']}"
-    #         f"\nШанс статуса: {data['attacks'][0]['status_chance']}"
-    #         f"\nУдар: {data['attacks'][0]['damage']['impact']}"
-    #         f"\nРазрез: {data['attacks'][0]['damage']['slash']}"
-    #         f"\nПронзание: {data['attacks'][0]['damage']['puncture']}"
-    #         )
-
-    # print(item)
-
-    # return  item
+# def process_search_choice(message):
+#     if message.text == "Искать следующий предмет":
+#         bot.send_message(message.chat.id, "Введите название следующего предмета для поиска:")
+#         bot.register_next_step_handler(message, get_item)
+#     elif message.text == "Назад":
+#         get_text_messages(message)
 
 
 def get_rivens():
@@ -134,16 +135,35 @@ def get_voidTrader():
     data = response.json()
     if data['active'] == True:
         voidTrader = (f"Локация: *{data['location']}\n*{data['inventory']}")
-
     else:
         voidTrader = (f"Баро Китир прибудет через: *{data['startString']}*\nЛокация: *{data['location']}*")
     return voidTrader
+
+
+def get_arbitration():
+    url = "https://api.warframestat.us/pc/ru/arbitration"
+    response = requests.get(url)
+    response.headers.get("Content-Type")
+    data = response.json()
+
+    end_date = (data['expiry'])
+    end_time = datetime.fromisoformat(end_date.replace("Z", "+00:00"))
+    current_time = datetime.now(pytz.timezone("Europe/Moscow"))
+    time_left = end_time - current_time
+    remaining_days = time_left.days
+    remaining_hours, remainder = divmod(time_left.seconds, 3600)
+    remaining_minutes, _ = divmod(remainder, 60)
+    remaining_time = (f"*До конца осталось:*\nДней: {remaining_days} | Часов: {remaining_hours} | Минут: {remaining_minutes}")
+    arbitration = (f"*{data['type']}*\n{data['node']}\n{data['enemy']}\n{remaining_time}")
+    print(arbitration)
+    return arbitration
 
 
 def get_worldstate_data():
     cycle =[]
     cycle.append(get_vallisCycle())
     cycle.append(get_cetusCycle())
+    cycle.append(get_cambionCycle())
     return cycle
 
 def get_vallisCycle():
@@ -171,6 +191,20 @@ def get_cetusCycle():
 
     return cetus_cycle
 
+
+def get_cambionCycle():
+    url = "https://api.warframestat.us/pc/ru/cambionCycle"
+    response = requests.get(url)
+    response.headers.get("Content-Type")
+    data = response.json()
+    if data['state'] == "vome":
+        data['state'] = "Воум"
+    else:
+        data['state'] = "Фэз"
+    cambion_cycle = (f"Камбионский дрейф: *{data['state']}*\nОсталось: *{data['timeLeft']}*")
+
+    return cambion_cycle
+
 def get_steel_path__reward():
     url = "https://api.warframestat.us/pc/ru/steelPath"
     response = requests.get(url)
@@ -178,7 +212,6 @@ def get_steel_path__reward():
     data = response.json()
     steel_path_reward = (f"*{data['currentReward']['name']}*\n*Стоимость*: {data['currentReward']['cost']} стали\n")
     return steel_path_reward
-
 
 
 def get_dat(mode):
@@ -211,14 +244,19 @@ def get_dat(mode):
 @bot.message_handler(commands=['start'])
 def start(message):
 
-    btn1 = types.KeyboardButton("Список разрывов бездны")
+    btn1 = types.KeyboardButton("Разрывы бездны")
     btn2 = types.KeyboardButton("Циклы мира")
     btn3 = types.KeyboardButton("Текущая награда стального пути")
-    btn4 = types.KeyboardButton("Узнать товары Баро Китира")
+    btn4 = types.KeyboardButton("Товары Баро Китира")
     btn5 = types.KeyboardButton("Найти предмет")
     btn6 = types.KeyboardButton("Текущие ивенты")
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True,row_width=1)
-    markup.add(btn1,btn2,btn3,btn4,btn5,btn6)
+    btn7 = types.KeyboardButton("Арбитраж")
+    if message.chat.id in subscribers:
+        btn8 = types.KeyboardButton("Отписаться от уведомлений")
+    else:
+        btn8 = types.KeyboardButton("Подписаться на уведомления")
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True,row_width=2)
+    markup.add(btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8)
     bot.send_message(message.from_user.id, "Привет,я бот-помощник для игры Warframe", reply_markup=markup)
 
 @bot.message_handler(content_types=['text'])
@@ -233,7 +271,7 @@ def get_text_messages(message):
         for i in range(len(data)):
             bot.send_message(message.from_user.id,data[i] , reply_markup=markup,parse_mode="Markdown")
 
-    if message.text =="Список разрывов бездны":
+    if message.text =="Разрывы бездны":
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
         btn2 = types.KeyboardButton("Cтальной путь")
         btn3 = types.KeyboardButton("Обычный режим")
@@ -266,17 +304,22 @@ def get_text_messages(message):
 
 
     if message.text == "Назад":
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True,row_width=1)
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True,row_width=2)
         btn1 = types.KeyboardButton("Циклы мира")
-        btn2 = types.KeyboardButton("Список разрывов бездны")
+        btn2 = types.KeyboardButton("Разрывы бездны")
         btn3 = types.KeyboardButton("Текущая награда стального пути")
-        btn4 = types.KeyboardButton("Узнать товары Баро Китира")
+        btn4 = types.KeyboardButton("Товары Баро Китира")
         btn5 = types.KeyboardButton("Найти предмет")
         btn6 = types.KeyboardButton("Текущие ивенты")
-        markup.add(btn1,btn2, btn3,btn4,btn5,btn6)
+        btn7 = types.KeyboardButton("Арбитраж")
+        if message.chat.id in subscribers:
+            btn8 = types.KeyboardButton("Отписаться от уведомлений")
+        else:
+            btn8 = types.KeyboardButton("Подписаться на уведомления")
+        markup.add(btn1,btn2, btn3,btn4,btn5,btn6,btn7,btn8)
         bot.send_message(message.from_user.id, "Выберите режим", reply_markup=markup)
 
-    if message.text == "Узнать товары Баро Китира":
+    if message.text == "Товары Баро Китира":
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
         btn1 = types.KeyboardButton("Назад")
         markup.add(btn1)
@@ -298,10 +341,70 @@ def get_text_messages(message):
         for i in range(len(data)):
             bot.send_message(message.from_user.id, data[i], reply_markup=markup, parse_mode="Markdown")
 
+    if message.text == "Арбитраж":
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn1 = types.KeyboardButton("Назад")
+        markup.add(btn1)
+        data = get_arbitration()
+        bot.send_message(message.from_user.id, data, reply_markup=markup, parse_mode="Markdown")
+
+    if message.text == "Подписаться на уведомления":
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn1 = types.KeyboardButton("Назад")
+        markup.add(btn1)
+        bot.send_message(message.from_user.id,'Вы подписываетесь на уведомления о разрывах бездны стального пути.\n\nВведите интервал уведомлений в минутах' , reply_markup=markup,parse_mode="Markdown")
+        bot.register_next_step_handler(message, set_notification_interval)
+
+    if message.text == "Отписаться от уведомлений":
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn1 = types.KeyboardButton("Назад")
+        markup.add(btn1)
+        bot.send_message(message.chat.id, "Вы отписались от уведомлений.", reply_markup=markup)
+        if message.chat.id in subscribers:
+            del subscribers[message.chat.id]
+            del notification_schedule[message.chat.id]
+
+def set_notification_interval(message):
+    chat_id = message.chat.id
+    if message.text.lower() == 'назад':
+        get_text_messages(message)
+        return
+    try:
+        minutes = int(message.text)
+        if minutes <= 0 or minutes>  720:
+            bot.send_message(chat_id, "Интервал должен быть положительным числом не более 720 минут.Пожалуйста, попробуйте снова.")
+            bot.register_next_step_handler(message, set_notification_interval)
+            return
+        subscribers[message.chat.id] = True
+        notification_schedule[chat_id] = minutes
+        bot.send_message(chat_id, f"Интервал для уведомлений успешно установлен на {minutes} минут.")
+        schedule.clear(tag=chat_id)
+        schedule_notification(chat_id, minutes)
+    except ValueError:
+        bot.send_message(chat_id, "Пожалуйста, введите целое число минут. Пожалуйста, попробуйте снова.")
+        bot.register_next_step_handler(message, set_notification_interval)
+
+
+def schedule_notification(chat_id, minutes):
+    schedule.every(minutes).minutes.do(send_notification, chat_id).tag(chat_id)
+
+def send_notification(chat_id):
+    if chat_id in subscribers:
+        data = get_dat("Cтальной путь")
+        for i in range(len(data)):
+            bot.send_message(chat_id, data[i], parse_mode="Markdown")
+
+def run_schedule():
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
 
 if __name__ == '__main__':
     while 1:
         try:
+            schedule_thread = Thread(target=run_schedule)
+            schedule_thread.start()
             bot.infinity_polling()
         except Exception as e:
             logging.error(e)
